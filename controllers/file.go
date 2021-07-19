db5da8fcb7ffad89a9b7275d54acaba471b1194b
jamesanto <james@socure.me>
2021-07-20 02:15:55 +0530

    test

--- controllers/file.go
+++ controllers/file.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +16,11 @@ import (
 var (
 	imgexp = regexp.MustCompile("^image")
 )
+
+type OcrRes struct {
+	Word       string
+	Confidence float64
+}
 
 // FileUpload ...
 func FileUpload(w http.ResponseWriter, r *http.Request) {
@@ -65,6 +71,21 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 	case "hocr":
 		out, err = client.HOCRText()
 		render.EscapeHTML = false
+	case "json":
+		var out1 []gosseract.BoundingBox
+		var out2 []byte
+		var out3 []OcrRes
+		out1, err = client.GetBoundingBoxes(gosseract.RIL_WORD)
+		if err != nil {
+			render.JSON(http.StatusBadRequest, err)
+			return
+		}
+		for _, x := range out1 {
+			ir := OcrRes{Word: x.Word, Confidence: x.Confidence}
+			out3 = append(out3, ir)
+		}
+		out2, err = json.Marshal(out3)
+		out = string(out2)
 	default:
 		out, err = client.Text()
 	}
